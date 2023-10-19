3abc0944e8faf37258d167c2399fda42804a84ce
develoHAM <kdw980722@gmail.com>
2023-10-19 15:14:11 +0900

    f

--- server/index.js
+++ server/index.js
@@ -66,12 +66,7 @@ const uploadImageMulterMiddleware = upload.single('file');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin: 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app/',
-		credentials: true,
-	})
-);
+app.use(cors());
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.use('/user', uploadImageMulterMiddleware, userRouter);
