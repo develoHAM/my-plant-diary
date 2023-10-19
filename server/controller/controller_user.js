b5007699b45371884c024cae278685fa7a3e171b
develoHAM <kdw980722@gmail.com>
2023-10-19 17:09:47 +0900

    delete res.headers

--- server/controller/controller_user.js
+++ server/controller/controller_user.js
@@ -21,8 +21,8 @@ const S3 = new AWS.S3({
 });
 
 controller.signup = async (req, res) => {
-	res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	// res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
+	// res.header('Access-Control-Allow-Credentials', 'true');
 	try {
 		const { userid, password, name, email } = req.body;
 		const duplicatedUserid = await Model.Users.findOne({
@@ -62,8 +62,8 @@ controller.signup = async (req, res) => {
 };
 
 controller.signin = async (req, res) => {
-	res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	// res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
+	// res.header('Access-Control-Allow-Credentials', 'true');
 	try {
 		const { userid, password } = req.body;
 		const user = await Model.Users.findOne({
@@ -97,8 +97,8 @@ controller.signin = async (req, res) => {
 };
 
 controller.update = async (req, res) => {
-	res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	// res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
+	// res.header('Access-Control-Allow-Credentials', 'true');
 	try {
 		const { id, userid, name, email, password, profile_pic } = req.body;
 		const pw = bcrypt.hashSync(password, SALT);
@@ -146,8 +146,8 @@ controller.update = async (req, res) => {
 };
 
 controller.updateprofilepic = async (req, res) => {
-	res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	// res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
+	// res.header('Access-Control-Allow-Credentials', 'true');
 	try {
 		const path = req.file.path || req.file.transforms[0].location;
 		console.log('path', path);
@@ -208,8 +208,8 @@ controller.updateprofilepic = async (req, res) => {
 };
 
 controller.authenticate = async (req, res) => {
-	res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	// res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
+	// res.header('Access-Control-Allow-Credentials', 'true');
 	try {
 		const token = req.cookies.login_token;
 		const userInfo = req.body;
@@ -249,8 +249,8 @@ controller.authenticate = async (req, res) => {
 };
 
 controller.submitpost = async (req, res) => {
-	res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	// res.header('Access-Control-Allow-Origin', 'https://web-my-plant-diary-jvpb2alnwnvncg.sel5.cloudtype.app');
+	// res.header('Access-Control-Allow-Credentials', 'true');
 	const path = req.file.path || req.file.transforms[0].location;
 	const token = req.cookies.login_token;
 	console.log('req.file', req.file);
